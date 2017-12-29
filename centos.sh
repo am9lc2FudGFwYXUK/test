543cec9c15104df41ef41a827e38f1a90af10e30
root <root@localhost.localdomain.localdomain>
2017-12-29 02:02:27 +0000

    some updates to the centos yum shell script

--- centos.sh
+++ centos.sh
@@ -3,6 +3,10 @@
 list="gcc gcc-c++ jansson-devel flex bison libxml2-devel libevent-devel libcurl-devel python-devel lua-devel"
 
 
+yum -y update yum 
+
+you -y update  
+
 for i in $list 
 	do 
 		yum -y install $i 
